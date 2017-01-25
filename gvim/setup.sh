02eb465a8826f499b95525a61fac6ce2e7f3ee0b
John Varga <John Varga>
2017-01-25 14:56:14 -0800

    add script for setting up pathogen; also update _vimrc for pathogen

---
+++ gvim/setup.sh
@@ -0,0 +1,9 @@
+# run with bash or conemu
+mkdir -p ~/vimfiles/autoload ~/vimfiles/bundle && \
+curl -LSso ~/vimfiles/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# add to _vimrc
+# synatx on
+# execute pathogen#infect()
+# filetype plugin indent on
+
