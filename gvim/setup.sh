d31237f8cf01be64fdd720959f892be4e7aa090d
John Varga <John Varga>
2017-01-25 14:57:19 -0800

    add airline to setup script

--- gvim/setup.sh
+++ gvim/setup.sh
@@ -7,3 +7,7 @@ curl -LSso ~/vimfiles/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 # execute pathogen#infect()
 # filetype plugin indent on
 
+# vim airline
+git clone https://github.com/vim-airline/vim-airline ~/vimfiles/bundle/vim-airline
+# remember to run :Helptags to generate help tags
+
