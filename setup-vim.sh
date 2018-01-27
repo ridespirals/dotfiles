000480c78bc5bff3a6cedddaab98ef631ae2d5a7
John Varga <john.varga@unifilabs.com>
2018-01-26 23:43:47 -0800

    add vim plugins to setup script

--- setup-vim.sh
+++ setup-vim.sh
@@ -1,8 +1,15 @@
 # copy vimfiles
 cp -r vimfiles ~/.vim
 
-# install Pathogen
+# install Pathogen, unimpaired, and fugitive
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+git clone https://github.com/tpope/vim-unimpaired.vim ~/.vim/bundle/vim-unimpaired
+git clone https://github.com/tpope/vim-fugitive.vim ~/.vim/bundle/vim-fugitive
 
 # install airline
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+
+# install fzf
+git clone https://github.com/junegunn/fzf.git ~/.vim/bundle/fzf
+git clone https://github.com/junegunn/fzf.vim.git ~/.vim/bundle/fzf.vim
+
