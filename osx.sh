9b0f0375ec698e138559647a9eae5c507879c549
John Varga <john.g.varga@gmail.com>
2018-01-27 23:11:54 -0800

    add notes to osx script for generating ssh key for github

--- osx.sh
+++ osx.sh
@@ -18,3 +18,13 @@ ln homebrew /usr/local/bin/bb
 # create a tmp folder for vim swap/backup files
 mkdir ~/.vim-tmp
 
+# SSH setup (for github)
+# ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+# eval "$(ssh-agent -s)" # start ssh agent in the background
+# edit ~/.ssh/config
+# Host *
+#   AddKeysToAgent yes
+#   UseKeychain yes
+#   IdentityFile ~/.ssh/id_rsa
+# ssh-add -K ~/.ssh/id_rsa
+
