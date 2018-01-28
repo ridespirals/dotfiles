2c837f48405d914fe43924da124fffead753dce9
John Varga <john.varga@unifilabs.com>
2018-01-27 20:30:39 -0800

    better vim setup script, can also update all plugins

--- setup-vim.sh
+++ setup-vim.sh
@@ -1,15 +1,57 @@
-# copy vimfiles
-cp -r vimfiles ~/.vim
+#!/bin/bash
 
-# install Pathogen, unimpaired, and fugitive
-mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-git clone https://github.com/tpope/vim-unimpaired.vim ~/.vim/bundle/vim-unimpaired
-git clone https://github.com/tpope/vim-fugitive.vim ~/.vim/bundle/vim-fugitive
+#############
+# Convenience script for all vim plugins.
+# - clone repo if doesn't exist
+# - otherwise, pull
+#############
 
-# install airline
-git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+if [ ! -e "${HOME}/.vim/autoload/pathogen.vim" ]
+then
+    # likely setting up vim for the first time. install pathogen
+    mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+fi
 
-# install fzf
-git clone https://github.com/junegunn/fzf.git ~/.vim/bundle/fzf
-git clone https://github.com/junegunn/fzf.vim.git ~/.vim/bundle/fzf.vim
+if [ ! -d "${HOME}/.vim/colors" ]
+then
+    # color schemes not in place, copy it over
+    # TODO: get colors online? copy from dotfiles?
+    echo "++ Colors not found! Go get zenburn somewhere."
+fi
+
+
+DIR="$HOME/.vim/bundle"
+PLUGINS=(
+    "tpope/vim-unimpaired" 
+    "tpope/vim-fugitive"
+    "tpope/vim-vinegar"
+    "tpope/vim-commentary"
+    "tpope/vim-characterize"
+    "tpope/vim-surround"
+    "vim-airline/vim-airline"
+    "junegunn/fzf"
+    "junegunn/fzf.vim"
+)
+for P in ${!PLUGINS[*]}
+do
+    REPO=${PLUGINS[P]}
+    PLUG_NAME=$(echo $REPO | cut -d'/' -f2)
+    PLUG_DIR="${DIR}/${PLUG_NAME}"
+    REPO_PATH="git@github.com:${REPO}.git"
+
+    if [ -d ${PLUG_DIR} ]
+    then
+        # cd and git pull
+        echo "^ Updating ${PLUG_NAME}"
+        cd $PLUG_DIR
+        git fetch
+        git pull > /dev/null
+    else
+        # clone repo
+        echo "+ Installing ${PLUG_NAME}"
+        git clone $REPO_PATH $PLUG_DIR
+        git clone "git@github.com:${REPO}.git" $PLUG_DIR
+    echo
+    fi
+done
 
